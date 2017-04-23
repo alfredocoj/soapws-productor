eb0be92985c514f3d74dbe4cabc91c5649dd6d64
Alfredo Oliveira <alfredo.coj@gmail.com>
2017-04-22 21:01:41 -0300

    Insticiacao de classe JPAConfiguration

--- src/main/java/br/ws/WssoapApplication.java
+++ src/main/java/br/ws/WssoapApplication.java
@@ -3,10 +3,14 @@ package br.ws;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.ws.conf.JPAConfiguration;
+
 @SpringBootApplication
 public class WssoapApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WssoapApplication.class, args);
+		
+		new JPAConfiguration();
 	}
 }
