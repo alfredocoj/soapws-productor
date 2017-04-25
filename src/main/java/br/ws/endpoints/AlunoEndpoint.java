2ed533187cefb7cd52fca68d6775e9ea0a50cfb3
Alfredo Oliveira <alfredo.coj@gmail.com>
2017-04-24 21:43:00 -0300

    criacao de metodo em addAluno em EndPoint.

--- src/main/java/br/ws/endpoints/AlunoEndpoint.java
+++ src/main/java/br/ws/endpoints/AlunoEndpoint.java
@@ -1,5 +1,7 @@
 package br.ws.endpoints;
 
+import br.ws.soap.PostAlunoRequest;
+import br.ws.soap.PostAlunoResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -26,6 +28,21 @@ public class AlunoEndpoint {
 		Aluno repository = (Aluno) alunoModel.getById(aluno);
 		response.setAluno(repository);
 		System.out.println(response.getAluno().getAlunoName());
+		return response;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "postAlunoRequest")
+	@ResponsePayload
+	public PostAlunoResponse addAluno(@RequestPayload PostAlunoRequest request) throws Exception {
+		PostAlunoResponse response = new PostAlunoResponse();
+
+		Aluno aluno = new Aluno();
+		aluno.setAlunoName( request.getAlunoName() );
+		aluno.setAlunoIdade( request.getAlunoIdade() );
+		aluno.setAlunoClasse( request.getAlunoClasse() );
+		Aluno repository = (Aluno) alunoModel.save( aluno );
+		response.setAluno(repository);
+
 		return response;
 	}
 }
